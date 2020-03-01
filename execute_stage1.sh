4d4868c5b67a2e81479e65a14e57f55afb6931a1
Avocadososemix <22445006+Avocadososemix@users.noreply.github.com>
2020-03-01 17:33:15 +0200

    updated stage1.sh

--- execute_stage1.sh
+++ execute_stage1.sh
@@ -1,6 +1,27 @@
+# A
+cd /home/cecuser/Project/stage1/
 git clone https://github.com/dockersamples/node-bulletin-board
 cd node-bulletin-board/bulletin-board-app
-
 docker image build -t bulletinboard:1.0 .
-
 docker container run --publish 8000:8080 --detach --name bb bulletinboard:1.0
+
+# B
+docker run -d --name redis-container -v /var/cec/redis.rdb:/data/dump.rdb redis:latest
+
+# C
+#cd /home/cecuser/Project/stage1/
+mkdir /home/cecuser/Project/stage1/flask_server
+cd /home/cecuser/Project/stage1/flask_server
+
+cat > /home/cecuser/Project/stage1/flask_server/flask_server.py << ENDOFFILE
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+ENDOFFILE
+
+export FLASK_APP=flask_server.py
+export FLASK_DEBUG=1
+python3.7 -m flask run &
