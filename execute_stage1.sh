64e9dd26ffeb3a650e9029fc4ca0b7382b811d05
Avocadososemix <22445006+Avocadososemix@users.noreply.github.com>
2020-03-01 19:55:49 +0200

    return factorial with flask

--- execute_stage1.sh
+++ execute_stage1.sh
@@ -15,28 +15,31 @@ sudo docker start redis-container
 kill -9 `lsof -i:5000 -t` # kill extra copies of flask running
 
 pip3 install -U Flask
+pip3 install redis
+pip3 install config
 #cd /home/cecuser/Project/stage1/
 mkdir -p /home/cecuser/Project/stage1/flask_server
 cd /home/cecuser/Project/stage1/flask_server
 
 cat > /home/cecuser/Project/stage1/flask_server/flask_server.py << ENDOFFILE
 
-
-
-
-
-
 from flask import Flask
 import redis
-from config import redis_uri
-
-r = redis.Redis(url='127.0.0.1:6379')
 
 app = Flask(__name__)
 
+r = redis.StrictRedis(host="localhost", port=6379, db=0)
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def factorial():
+    value = r.get(r.randomkey()) 
+    text = 'Factorial of '
+    text += str(int(value))
+    text += ' " '
+    factorial = 1
+    for i in range(1,int(value)+1:
+        factorial = factorial * i
+    text += str(factorial)
+    return text
 ENDOFFILE
 
 export FLASK_APP='flask_server.py'
