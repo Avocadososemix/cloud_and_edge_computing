944efb8bc2ae2251b7f9ea5e29228a952003dd37
Avocadososemix <22445006+Avocadososemix@users.noreply.github.com>
2020-03-01 17:36:50 +0200

    fix

--- execute_stage1.sh
+++ execute_stage1.sh
@@ -1,7 +1,7 @@
 # A
 cd /home/cecuser/Project/stage1/
 git clone https://github.com/dockersamples/node-bulletin-board
-cd node-bulletin-board/bulletin-board-app
+cd /home/cecuser/Project/stage1/node-bulletin-board/bulletin-board-app
 docker image build -t bulletinboard:1.0 .
 docker container run --publish 8000:8080 --detach --name bb bulletinboard:1.0
 
